b048a9dec3d391ac6ec0d48dd31b0ab04936d2a6
lgonzalez-ac <gonzalez.lenin.ac@gmail.com>
2025-11-29 10:13:18 -0400

    permisos agregados en Crar publicacion y update

--- blogs/views.py
+++ blogs/views.py
@@ -4,19 +4,20 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView 
 from django.urls import reverse_lazy
 # Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class PublicationListView(ListView):
     model = Publication
     template_name = 'home.html'
 
-class PublicationCreteView(CreateView):
+class PublicationCreteView(LoginRequiredMixin, CreateView):
     model = Publication
     template_name = 'create.html'
     fields = ['title', 'content', 'author']
     success_url = reverse_lazy('publications-list')
 
-class PublicationDetailView(DetailView):
+class PublicationDetailView(LoginRequiredMixin, DetailView):
     model = Publication
     template_name = 'publication_detail.html'    
     
